4affea3ab59c7c40ff485bdbbd20b1862e77f4b1
Supernova1024 <snow423star@gmail.com>
2018-02-07 12:54:34 -0500

    adds edits to fix corrections to run app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-import Home from "./pages/Companies";
+import Companies from "./pages/Companies";
 import Header from "./components/Header";
 
 const App = () =>
--- client/src/pages/Companies/Companies.js
+++ client/src/pages/Companies/Companies.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
-import Radio_Button from "../../components/Radio_Button";
+// import Radio_Button from "../../components/Radio_Button";
 import Jumbotron from "../../components/Jumbotron";
 
 
-class Companies extends Components {
+class Companies extends Component{
 
 state = {
 	companyList:["Home Depot", "Target", "Walmart", "Publix", "Amazon", "Wendys"]
@@ -26,7 +26,7 @@ componentDidMount(){
 							<h1>Companies</h1>
 							<ul>
 							{this.state.companyList.map(company => (
-                  			<li><RadioButton/>{company}</li>
+                  			<li>{company}</li>
                   			))}
                   			</ul>
                     		<button>One</button>
