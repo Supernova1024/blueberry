a49738c6f78ddb130a000dd634fb36ef4609480d
Supernova1024 <snow423star@gmail.com>
2018-02-07 13:25:16 -0500

    Added AddCompanies pages and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-import Home from "./pages/Companies";
+import Companies from "./pages/Companies";
+import AddCompany from "./pages/AddCompany";
+import Sidebar from "./pages/Sidebar";
+import Footer from "./pages/Footer";
 import Header from "./components/Header";
 
 const App = () =>
   <Router>
     <div>
       <Header/>
+      <Sidebar/>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/companies" component={Companies} />
+        <Route exact path="/addcompany" component={AddCompany} />
       </Switch>
+      <Footer className="footer"/>
     </div>
   </Router>;
 
