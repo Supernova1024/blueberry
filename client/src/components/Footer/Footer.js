3030b1c2fe36ce1f65d2827966b08432fffcba9a
Supernova1024 <snow423star@gmail.com>
2018-02-07 23:34:17 -0500

    corrections to footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,18 +1,24 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = ({topText, bottomText}) => (
-  <footer
+  <footer className="footer"
+    
     style={{
-      color: 'gray',
+      background: '#4F86F7',
+      color: 'white',
       fontFamily: 'Helvetica',
-      textAlign: 'center',
+      textAlign: 'center'
+
     }}
   >
+    <div className="container-fluid">
   	<span style={{ fontWeight: 'bold' }}>
     	{topText}
     </span>
     <br />
     {bottomText}
+    </div>
   </footer>
 );
 
