65a5b2a58c62964b846d3d587712750680c90410
Supernova1024 <snow423star@gmail.com>
2018-02-08 12:28:13 -0500

    Connected client to server to post new companies

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Button.css";
 // Destructuring the type, className, children and onClick props, applying them to the button element
-const Button = ({ type = "primary", className, children }) => (
+const Button = ({ type = "primary", className, children, onClick }) => (
   <button id="form-button"
-    
+  	onClick={onClick}
     className={["btn", `btn-${type} myBtn`, className].join(" ")}
   >
     {children}
