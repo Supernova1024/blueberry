b6823566b1fed241bcdb5336015485c0e7bbd1fb
Supernova1024 <snow423star@gmail.com>
2018-02-08 10:05:35 -0500

    server api and client routing

--- client/src/pages/AddCompany/AddCompany.js
+++ client/src/pages/AddCompany/AddCompany.js
@@ -6,6 +6,9 @@ import Jumbotron from "../../components/Jumbotron";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import "./AddCompany.css";
+import API from "../../utils/API";
+  
+
 
 class AddCompany extends Component {
 
@@ -33,6 +36,28 @@ componentDidMount(){
       [name]: value
     });
   };
+
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (event !== 5) {
+      API.saveCompany({
+		    tin: this.state.title,
+			companyName: this.state.companyName,
+			address: this.state.address,
+			city: this.state.city,
+			state: this.state.state,
+			zip: this.state.zip,
+			contactName: this.state.contactName,
+			contactPhone: this.state.contactPhone,
+			contactEmail: this.state.contactEmail,
+      })
+        .then(res => this.loadBooks())
+        .catch(err => console.log(err));
+    }
+  };
+
+
 
 	render(){
 		return (
