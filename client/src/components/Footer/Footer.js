6b4eb21e2a9e2a9b66fb7f9938f29ebc4fe85a84
Supernova1024 <snow423star@gmail.com>
2018-02-12 14:35:58 -0500

    added social media icons

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -28,6 +28,17 @@ function Footer() {
             <div style={phantom}>
                 <div style={style}>
                     Our Company - Help - Privacy 
+
+    
+
+        <div class="container">
+        <div class="text-center center-block">
+                <a href="https://www.facebook.com/bootsnipp"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+	            <a href="https://twitter.com/bootsnipp"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+	            <a href="https://plus.google.com/+Bootsnipp-page"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+	            <a href="mailto:#"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+    </div>
+</div>
                 </div>  
             </div>
         </div>
