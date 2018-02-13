a6213a46a739092ea1701b3c05f2f7b2da2aac18
Supernova1024 <snow423star@gmail.com>
2018-02-13 13:18:44 -0500

    modifies post-build script for Heroku in package.json

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Companies from "./pages/Companies";
-import Vendors from "./pages/Vendors"
+import Vendors from "./pages/Vendors";
 import Login from "./pages/Login";
 import AddCompany from "./pages/AddCompany";
 
 import DisplayPdf from "./pages/DisplayPdf";
 
-import AddVendor from  "./pages/AddVendor";
+import AddVendor from "./pages/AddVendor";
 
 import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
@@ -16,40 +16,35 @@ import Splashpage from "./components/Splashpage";
 
 import Header from "./components/Header";
 import Jumbotron from "./components/Jumbotron";
-const App = () =>
+const App = () => (
   <Router>
     <div>
-
-      <Header/>
-       <div className="container-fluid">
-  <div className="row">
-    <div className="col-md-3">
-      <Sidebar/>
-      </div>
+      <Header />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-3">
+            <Sidebar />
+          </div>
           <div className="col-md-6">
-      <Switch>
-          <Jumbotron id="main">
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route excat path="/SplashPage" component ={Splashpage}/>
-        <Route exact path="/companies" component={Companies} />
-        <Route exact path="/addcompany" component={AddCompany} />
-        <Route exact path="/displaypdf" component={DisplayPdf} />
-        <Route exact path="/companies/:id" component={Vendors} />
-        <Route excat path="/addvendor" component={AddVendor}/>
-
-
-        </Jumbotron>
-      </Switch>
-      </div>
-        <div className="col-md-3">
+            <Switch>
+              <Jumbotron id="main">
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route excat path="/SplashPage" component={Splashpage} />
+                <Route exact path="/companies" component={Companies} />
+                <Route exact path="/addcompany" component={AddCompany} />
+                <Route exact path="/displaypdf" component={DisplayPdf} />
+                <Route exact path="/companies/:id" component={Vendors} />
+                <Route excat path="/addvendor" component={AddVendor} />
+              </Jumbotron>
+            </Switch>
+          </div>
+          <div className="col-md-3" />
         </div>
       </div>
-      
+      <Footer />
     </div>
-    <Footer />
-    </div>
-
-  </Router>;
+  </Router>
+);
 
 export default App;
