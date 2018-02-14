555cf91b5a6952dffa2a0f3fa208e6d04ae79855
Supernova1024 <snow423star@gmail.com>
2018-02-14 00:09:35 -0500

    updated pdf page

--- client/src/pages/DisplayPdf/DisplayPdf.js
+++ client/src/pages/DisplayPdf/DisplayPdf.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PDF from 'react-pdf-js';
 import "./DisplayPdf.css"
 import MyPDF from "./2017-1099-MISC.jpg"
+import Input from "../../components/Input";
 
 
  
@@ -14,7 +15,26 @@ class DisplayPdf extends React.Component {
   render() {
     
     return (
-      <img id = "pdf" src={MyPDF}/>
+    	<div className="jumbotron form">
+
+   		 	<div className="company-name">
+    		 <p> {"The Home Depot"} </p> 
+    		 <p> {"142 Paces Ferry Rd"} </p>
+    		 <p> {"Atlanta, GA 30039"}  </p> 
+      
+      		</div>
+              <table>
+              <tr>
+      		<td className="vendor-name">
+      		 Bill Jones 
+      		 </td>
+      		     <td className="amount"> 45,000.00 </td>
+      		     </tr>
+      		     </table>
+      		
+
+      </div>
+
        
         
       
